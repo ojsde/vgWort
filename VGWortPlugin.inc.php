17e6a17c6f08ed50eac682dfe781abf04c3d0f27
ronste <root@localhost>
2021-03-12 15:01:22 +0000

    fix

--- VGWortPlugin.inc.php
+++ VGWortPlugin.inc.php
@@ -672,7 +672,7 @@ class VGWortPlugin extends GenericPlugin {
 		$journal = $smarty->get_template_vars('currentJournal');
 		$issue = $smarty->get_template_vars('issue');
 		$publishedArticles = $smarty->get_template_vars('publishedArticles');
-		if (exists($issue) ? $issue->getPublished() : false) {
+		if (isset($issue) ? $issue->getPublished() : false) {
 			$scriptInserted = false;
 			foreach ($publishedArticles as $sectionId) {
 				foreach ($sectionId['articles'] as $publishedArticle) {
